9d8559d15137108eec9e8f1e4ec81dac5327d63d
joepaji <rjoheb@hotmail.com>
2019-05-31 22:54:51 -0700

    PlayerController and AI Controller classes created

---
+++ BattleTank/Source/BattleTank/Private/TankAIController.cpp
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "TankAIController.h"
+
+void ATankAIController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Possessed AI Tank Name: %s"), *(ControlledTank->GetName()));
+	}
+
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("No Possessed AI Tanks Found!"));
+	}
+}
+
+ATank* ATankAIController::GetControlledTank() const
+{
+	return Cast<ATank>(GetPawn());
+}
