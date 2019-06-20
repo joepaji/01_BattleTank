79a1ab5cc1607faa1e969795b84734e912cc7515
joepaji <rjoheb@hotmail.com>
2019-06-19 18:35:50 -0700

    Basic Movement controls using A and D keys + Some messing around with collisions + Known Bug: This is a movement bug, random colliions (Possible solution would be in the next few commits that would include fly-by-wire implementation)

--- BattleTank/Source/BattleTank/Public/Tracks.h
+++ BattleTank/Source/BattleTank/Public/Tracks.h
@@ -7,9 +7,9 @@
 #include "Tracks.generated.h"
 
 /**
- * 
+ * Tracks is used to set max driving force and apply forces to the tank
  */
-UCLASS(meta=(BlueprintSpawnableComponent), hideCategories = ("Collision"))
+UCLASS(meta=(BlueprintSpawnableComponent))
 class BATTLETANK_API UTracks : public UStaticMeshComponent
 {
 	GENERATED_BODY()
@@ -17,4 +17,8 @@ class BATTLETANK_API UTracks : public UStaticMeshComponent
 public:
 	UFUNCTION(BlueprintCallable, Category = Input)
 	void SetThrottle(float Throttle);
+
+	// Max driving force per track, in newtons.
+	UPROPERTY(EditDefaultsOnly)
+	float TrackMaxDrivingForce = 400000; /// Assume 48 ton tank, 1g force/10m/s^2 acceleration
 };
